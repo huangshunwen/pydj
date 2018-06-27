0c14c211195d88a5a85084d6a44db4f2506b89ab
huangshunwen <1006695924@qq.com>
2018-06-27 15:28:00 +0800

    first commit

---
+++ mysit/sign/views.py
@@ -0,0 +1,113 @@
+#coding=utf-8
+from django.contrib.auth.decorators import login_required
+from django.core.paginator import Paginator, PageNotAnInteger, EmptyPage
+from django.http import HttpResponse, HttpResponseRedirect
+from django.shortcuts import render, get_object_or_404
+from django.contrib import auth
+
+# Create your views here.
+from sign.models import Event, Guest
+
+
+def index(request):
+    return render(request, "index.html")
+
+
+def login_action(request):
+    if request.method == 'POST':
+        username = request.POST.get('username', '')
+        password = request.POST.get('password', '')
+        user = auth.authenticate(username=username, password=password)
+        if user is not None:
+            auth.login(request, user)  #登录
+            response = HttpResponseRedirect('/event_manage/')
+            request.session['user'] = username
+            return response
+        else:
+            return render(request, 'index.html', {'error': 'username or password error!'})
+    else:
+        return render(request, 'index.html', {'error': 'username or password error!'})
+
+
+#发布会管理
+#@login_required
+def event_manage(request):
+    event_list = Event.objects.all()
+    username = request.session.get('user', '')
+    return render(request, 'event_manage.html', {"user": username, "events": event_list})
+
+
+def search_name(request):
+    username = request.session.get('user', '')
+    search_name = request.GET.get('name', '')
+    event_list = Event.objects.filter(name__contains=search_name)
+    return render(request, "event_manage.html", {"user": username, "events": event_list})
+
+
+#嘉宾管理
+#@login_required
+def guest_manage(request):
+    guest_list = Guest.objects.all()
+    username = request.session.get('user', '')
+    paginator = Paginator(guest_list, 2)
+    page = request.GET.get('page')
+    try:
+        contacts = paginator.page(page)
+    except PageNotAnInteger:
+        #if page is not an integer, deliver first page.
+        contacts = paginator.page(1)
+    except EmptyPage:
+        #if page is out of range (e.g.9999), deliver last page.
+        contacts = paginator.page(paginator.num_pages)
+    return render(request, 'guest_manage.html', {"user": username, "guests": contacts})
+
+
+def search_realname(request):
+    username = request.session.get('user', '')
+    search_realname = request.GET.get('realname', '')
+    guest_list = Guest.objects.filter(realname__contains=search_realname)
+    paginator = Paginator(guest_list, 2)
+    page = request.GET.get('page')
+    try:
+        contacts = paginator.page(page)
+    except PageNotAnInteger:
+        #if page is not an integer, deliver first page.
+        contacts = paginator.page(1)
+    except EmptyPage:
+        #if page is out of range (e.g.9999), deliver last page.
+        contacts = paginator.page(paginator.num_pages)
+    return render(request, "guest_manage.html", {"user": username, "guests": contacts})
+
+
+#签到页面
+#@login_required
+def sign_index(request, event_id):
+    event = get_object_or_404(Event, id=event_id)
+    return render(request, 'sign_index.html', {'event': event})
+
+
+# 签到动作
+#@login_required
+def sign_index_action(request, event_id):
+    event = get_object_or_404(Event, id=event_id)
+    phone = request.POST.get('phone', ' ')
+    result = Guest.objects.filter(phone=phone)
+    if not result:
+        return render(request, 'sign_index.html', {'event': event, 'hint': 'phone error.'})
+    result = Guest.objects.filter(phone=phone, event_id=event_id)
+    if not result:
+        return render(request, 'sign_index.html', {'event': event, 'hint': 'event id or phone error.'})
+    result = Guest.objects.get(phone=phone, event_id=event_id)
+    if result.sign:
+        return render(request, 'sign_index.html', {'event': event, 'hint': "user has sign in."})
+    else:
+        Guest.objects.filter(phone=phone, event_id=event_id).update(sign='1')
+        return render(request, 'sign_index.html', {'event': event, 'hint': 'sign in success! ', 'guest': result})
+
+
+#退出登录
+#@login_required
+def logout(request):
+    auth.logout(request)
+    response = HttpResponseRedirect('/index/')
+    return response
